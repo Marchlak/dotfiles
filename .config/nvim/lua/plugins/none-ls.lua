1fec16cf1fafb17f548182d56f9efc65390c0cbb
Marchlak <bingor1@wp.pl>
2025-04-13 13:57:28 +0200

    update nvim

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -4,23 +4,23 @@ return {
 		config = function()
 			require("mason-null-ls").setup({
 				ensure_installed = {
-					"stylua", -- Lua
-					"prettier", -- JavaScript/TypeScript
+					"stylua",
+					"prettier",
 				},
 			})
 		end,
 	},
 	{
 		"nvimtools/none-ls.nvim",
-    dependiencies = {
-      "nvimtools/none-ls-extras.nvim"
-    },
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim"
+		},
 		config = function()
 			local null_ls = require("null-ls")
 			null_ls.setup({
 				sources = {
-          null_ls.builtins.formatting.isort,
-          require("none-ls.diagnostics.eslint_d"),
+					null_ls.builtins.formatting.isort,
+					require("none-ls.diagnostics.eslint_d"),
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.prettier,
 					null_ls.builtins.formatting.black,
