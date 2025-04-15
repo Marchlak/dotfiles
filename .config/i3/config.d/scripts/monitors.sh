21b87b28329be0f7ac431b7a57b58c1a03934ac5
Marchlak <bingor1@wp.pl>
2025-04-15 22:33:25 +0200

    update

--- .config/i3/config.d/scripts/monitors.sh
+++ .config/i3/config.d/scripts/monitors.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-xrandr --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --primary
-xrandr --output DisplayPort-1 --mode 1920x1080 --pos 1920x0
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 384
+# xrandr --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --primary
+# xrandr --output DisplayPort-1 --mode 1920x1080 --pos 1920x0
+# xrandr --output DVI-D-0 --mode 1920x1080 --pos 384
+xrandr --output HDMI-1-0 --auto --right-of eDP
