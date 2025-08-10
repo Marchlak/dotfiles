fbd6cee0b884177ff29b5b5533738ea4062a94be
Tomasz Marchela <bingor1@wp.pl>
2025-08-10 18:15:35 +0200

    update

--- home/.config/i3/config.d/scripts/rofi-wifi.sh
+++ home/.config/i3/config.d/scripts/rofi-wifi.sh
@@ -1,19 +1,46 @@
 #!/usr/bin/env bash
 THEME="$HOME/.config/i3/rofi/dark1.rasi"
+
 device=$(nmcli device status | awk '/wifi/ {print $1; exit}')
-mapfile -t SSIDS < <(nmcli -t -f SSID dev wifi list ifname "$device" | sed '/^$/d' | sort -u)
-ssid=$(printf '%s\n' "${SSIDS[@]}" | rofi -dmenu -i -p "WiFi" -theme "$THEME")
-if [ -n "$ssid" ]; then
-  sec=$(nmcli -t -f SSID,SECURITY dev wifi list ifname "$device" | grep "^$ssid:" | cut -d: -f2)
+current_ssid=$(nmcli -t -f ACTIVE,SSID dev wifi | awk -F: '$1=="yes"{print $2; exit}')
+mapfile -t WIFI_LIST < <(nmcli -t -f SSID,SIGNAL dev wifi list ifname "$device" | sed '/^:/d;/^$/d' | awk -F: '!seen[$1]++')
+
+get_icon() {
+    local signal=$1
+    if   [ "$signal" -ge 75 ]; then echo "󰤨"
+    elif [ "$signal" -ge 50 ]; then echo "󰤥"
+    elif [ "$signal" -ge 25 ]; then echo "󰤢"
+    elif [ "$signal" -ge 1  ]; then echo "󰤟"
+    else echo "󰤯"
+    fi
+}
+
+menu_list=""
+for entry in "${WIFI_LIST[@]}"; do
+    ssid=$(echo "$entry" | cut -d: -f1)
+    signal=$(echo "$entry" | cut -d: -f2)
+    [ -z "$ssid" ] && continue
+    icon=$(get_icon "$signal")
+    if [ "$ssid" = "$current_ssid" ]; then
+        menu_list+="$icon  $ssid (connected)\n"
+    else
+        menu_list+="$icon  $ssid\n"
+    fi
+done
+
+chosen=$(printf "%b" "$menu_list" | rofi -dmenu -i -p "WiFi" -theme "$THEME" | sed 's/^[^ ]\+[ ]\+//' | sed 's/ (connected)$//')
+
+if [ -n "$chosen" ]; then
+  sec=$(nmcli -t -f SSID,SECURITY dev wifi list ifname "$device" | grep "^$chosen:" | cut -d: -f2)
   if [[ "$sec" == *"802-1x"* || "$sec" == *"EAP"* ]]; then
-    identity=$(printf "" | rofi -dmenu -p "Username for $ssid" -theme "$THEME")
-    pass=$(printf "" | rofi -dmenu -password -p "Password for $ssid" -theme "$THEME")
-    nmcli connection add type wifi con-name "$ssid" ifname "$device" ssid "$ssid" wifi-sec.key-mgmt wpa-eap 802-1x.eap peap 802-1x.phase2-auth mschapv2 802-1x.identity "$identity" 802-1x.password "$pass" 802-1x.system-ca-certs yes
-    nmcli connection up "$ssid"
+    identity=$(printf "" | rofi -dmenu -p "Username for $chosen" -theme "$THEME")
+    pass=$(printf "" | rofi -dmenu -password -p "Password for $chosen" -theme "$THEME")
+    nmcli connection add type wifi con-name "$chosen" ifname "$device" ssid "$chosen" wifi-sec.key-mgmt wpa-eap 802-1x.eap peap 802-1x.phase2-auth mschapv2 802-1x.identity "$identity" 802-1x.password "$pass" 802-1x.system-ca-certs yes
+    nmcli connection up "$chosen"
   elif [ "$sec" != "--" ]; then
-    pass=$(printf "" | rofi -dmenu -password -p "Password for $ssid" -theme "$THEME")
-    nmcli dev wifi connect "$ssid" password "$pass" ifname "$device"
+    pass=$(printf "" | rofi -dmenu -password -p "Password for $chosen" -theme "$THEME")
+    nmcli dev wifi connect "$chosen" password "$pass" ifname "$device"
   else
-    nmcli dev wifi connect "$ssid" ifname "$device"
+    nmcli dev wifi connect "$chosen" ifname "$device"
   fi
 fi
