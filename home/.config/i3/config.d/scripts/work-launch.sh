0a02dcbe19e128af8bec1d7e52d0d348f44b2b80
Tomasz Marchela <bingor1@wp.pl>
2025-09-30 15:51:48 +0200

    update

--- home/.config/i3/config.d/scripts/work-launch.sh
+++ home/.config/i3/config.d/scripts/work-launch.sh
@@ -48,16 +48,20 @@ i3-msg 'workspace number 4; exec --no-startup-id ticktick --no-sandbox --passwor
 i3-msg 'workspace number 5; exec --no-startup-id spotify' >/dev/null
 i3-msg 'workspace number 6; exec --no-startup-id ~/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/bin/idea.sh' >/dev/null
 i3-msg 'workspace number 7; exec --no-startup-id gtk-launch pgadmin4.desktop' >/dev/null
+i3-msg 'workspace number 9; exec --no-startup-id brave-browser --app=https://teams.microsoft.com' >/dev/null
 i3-msg 'workspace number 10; exec --no-startup-id slack' >/dev/null
 
 sleep 4
 
-i3-msg '[class="(?i)brave-browser|brave"] move container to workspace number 2' >/dev/null
 i3-msg '[class="(?i)obsidian"] move container to workspace number 3' >/dev/null
 i3-msg '[class="(?i)ticktick"] move container to workspace number 4' >/dev/null
 i3-msg '[class="(?i)spotify"] move container to workspace number 5' >/dev/null
 i3-msg '[class="(?i)jetbrains-idea|intellij idea"] move container to workspace number 6' >/dev/null
 i3-msg '[class="(?i)slack"] move container to workspace number 10' >/dev/null
+
+i3-msg '[class="(?i)brave.*" title="(?i).*teams.*"] floating disable' >/dev/null
+i3-msg '[class="(?i)brave.*" title="(?i).*teams.*"] move container to workspace number 9' >/dev/null
+i3-msg '[class="(?i)brave.*" title="(?i)^(?!.*teams).*$"] move container to workspace number 2' >/dev/null
 
 sleep 1
 log "final-info: $(ghostty_info | tr -d '\n')"
